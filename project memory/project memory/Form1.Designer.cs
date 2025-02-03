756df28688ac1940f5da2233d9dd86a30a8dba49
dennis5132 <dennisvkrevel@gmail.com>
2025-02-03 14:30:47 +0100

    first draft of code

--- project memory/project memory/Form1.Designer.cs
+++ project memory/project memory/Form1.Designer.cs
@@ -28,13 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cardbtn1 = new System.Windows.Forms.Button();
+            this.cardbtn2 = new System.Windows.Forms.Button();
+            this.cardbtn3 = new System.Windows.Forms.Button();
+            this.cardbtn4 = new System.Windows.Forms.Button();
+            this.cardbtn5 = new System.Windows.Forms.Button();
+            this.cardbtn6 = new System.Windows.Forms.Button();
+            this.cardbtn7 = new System.Windows.Forms.Button();
+            this.cardbtn8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cardbtn1
+            // 
+            this.cardbtn1.Location = new System.Drawing.Point(170, 49);
+            this.cardbtn1.Name = "cardbtn1";
+            this.cardbtn1.Size = new System.Drawing.Size(111, 96);
+            this.cardbtn1.TabIndex = 0;
+            this.cardbtn1.Text = "button1";
+            this.cardbtn1.UseVisualStyleBackColor = true;
+            this.cardbtn1.Click += new System.EventHandler(this.cardClick1);
+            // 
+            // cardbtn2
+            // 
+            this.cardbtn2.Location = new System.Drawing.Point(348, 50);
+            this.cardbtn2.Name = "cardbtn2";
+            this.cardbtn2.Size = new System.Drawing.Size(110, 97);
+            this.cardbtn2.TabIndex = 1;
+            this.cardbtn2.Text = "button2";
+            this.cardbtn2.UseVisualStyleBackColor = true;
+            this.cardbtn2.Click += new System.EventHandler(this.cardbtn2_Click);
+            // 
+            // cardbtn3
+            // 
+            this.cardbtn3.Location = new System.Drawing.Point(527, 49);
+            this.cardbtn3.Name = "cardbtn3";
+            this.cardbtn3.Size = new System.Drawing.Size(105, 96);
+            this.cardbtn3.TabIndex = 2;
+            this.cardbtn3.Text = "button3";
+            this.cardbtn3.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn4
+            // 
+            this.cardbtn4.Location = new System.Drawing.Point(170, 171);
+            this.cardbtn4.Name = "cardbtn4";
+            this.cardbtn4.Size = new System.Drawing.Size(100, 89);
+            this.cardbtn4.TabIndex = 3;
+            this.cardbtn4.Text = "button4";
+            this.cardbtn4.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn5
+            // 
+            this.cardbtn5.Location = new System.Drawing.Point(348, 171);
+            this.cardbtn5.Name = "cardbtn5";
+            this.cardbtn5.Size = new System.Drawing.Size(110, 89);
+            this.cardbtn5.TabIndex = 4;
+            this.cardbtn5.Text = "button5";
+            this.cardbtn5.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn6
+            // 
+            this.cardbtn6.Location = new System.Drawing.Point(527, 171);
+            this.cardbtn6.Name = "cardbtn6";
+            this.cardbtn6.Size = new System.Drawing.Size(105, 89);
+            this.cardbtn6.TabIndex = 5;
+            this.cardbtn6.Text = "button6";
+            this.cardbtn6.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn7
+            // 
+            this.cardbtn7.Location = new System.Drawing.Point(266, 290);
+            this.cardbtn7.Name = "cardbtn7";
+            this.cardbtn7.Size = new System.Drawing.Size(98, 87);
+            this.cardbtn7.TabIndex = 6;
+            this.cardbtn7.Text = "button7";
+            this.cardbtn7.UseVisualStyleBackColor = true;
+            // 
+            // cardbtn8
+            // 
+            this.cardbtn8.Location = new System.Drawing.Point(443, 290);
+            this.cardbtn8.Name = "cardbtn8";
+            this.cardbtn8.Size = new System.Drawing.Size(98, 88);
+            this.cardbtn8.TabIndex = 7;
+            this.cardbtn8.Text = "button8";
+            this.cardbtn8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cardbtn8);
+            this.Controls.Add(this.cardbtn7);
+            this.Controls.Add(this.cardbtn6);
+            this.Controls.Add(this.cardbtn5);
+            this.Controls.Add(this.cardbtn4);
+            this.Controls.Add(this.cardbtn3);
+            this.Controls.Add(this.cardbtn2);
+            this.Controls.Add(this.cardbtn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cardbtn1;
+        private System.Windows.Forms.Button cardbtn2;
+        private System.Windows.Forms.Button cardbtn3;
+        private System.Windows.Forms.Button cardbtn4;
+        private System.Windows.Forms.Button cardbtn5;
+        private System.Windows.Forms.Button cardbtn6;
+        private System.Windows.Forms.Button cardbtn7;
+        private System.Windows.Forms.Button cardbtn8;
     }
 }
 
--- project memory/project memory/Form1.cs
+++ project memory/project memory/Form1.cs
@@ -16,5 +16,15 @@ namespace project_memory
         {
             InitializeComponent();
         }
+
+        private void cardClick1(object sender, EventArgs e)
+        {
+            cardbtn1.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+        }
+
+        private void cardbtn2_Click(object sender, EventArgs e)
+        {
+            cardbtn2.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+        }
     }
 }
