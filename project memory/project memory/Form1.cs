8018effbf4d98f4ea5aee7d9c2bad4b50d4fb54a
dennis5132 <dennisvkrevel@gmail.com>
2025-02-03 14:36:24 +0100

    stuff

--- project memory/project memory/Form1.cs
+++ project memory/project memory/Form1.cs
@@ -10,8 +10,11 @@ using System.Windows.Forms;
 
 namespace project_memory
 {
+    
     public partial class Form1 : Form
     {
+        float cardcheck = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,11 +23,14 @@ namespace project_memory
         private void cardClick1(object sender, EventArgs e)
         {
             cardbtn1.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+
+          
         }
 
         private void cardbtn2_Click(object sender, EventArgs e)
         {
             cardbtn2.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
         }
+        
     }
 }
