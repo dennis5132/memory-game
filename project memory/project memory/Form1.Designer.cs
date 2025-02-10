784dacaf0a13f34a4456fa72306c82b0bcbdb8cc
dennis5132 <dennisvkrevel@gmail.com>
2025-02-10 14:07:15 +0100

    fixed new picturemethod

--- project memory/project memory/Form1.Designer.cs
+++ project memory/project memory/Form1.Designer.cs
@@ -30,22 +30,26 @@
         {
             this.components = new System.ComponentModel.Container();
             this.resetTimer = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.cardbtn1 = new System.Windows.Forms.PictureBox();
+            this.cardbtn2 = new System.Windows.Forms.PictureBox();
+            this.cardbtn3 = new System.Windows.Forms.PictureBox();
+            this.cardbtn4 = new System.Windows.Forms.PictureBox();
+            this.cardbtn5 = new System.Windows.Forms.PictureBox();
+            this.cardbtn6 = new System.Windows.Forms.PictureBox();
+            this.cardbtn7 = new System.Windows.Forms.PictureBox();
+            this.cardbtn8 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.currentTurnLabel = new System.Windows.Forms.Label();
+            this.endTimer = new System.Windows.Forms.Timer(this.components);
+            this.pointsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn8)).BeginInit();
             this.SuspendLayout();
             // 
             // resetTimer
@@ -53,133 +57,168 @@
             this.resetTimer.Interval = 1;
             this.resetTimer.Tick += new System.EventHandler(this.resetTimer_Tick);
             // 
-            // pictureBox1
+            // cardbtn1
             // 
-            this.pictureBox1.AccessibleName = "pbx1";
-            this.pictureBox1.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
-            this.pictureBox1.Location = new System.Drawing.Point(147, 34);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.cardbtn1.AccessibleName = "pbx1";
+            this.cardbtn1.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+            this.cardbtn1.Location = new System.Drawing.Point(147, 34);
+            this.cardbtn1.Name = "cardbtn1";
+            this.cardbtn1.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn1.TabIndex = 8;
+            this.cardbtn1.TabStop = false;
             // 
-            // pictureBox2
+            // cardbtn2
             // 
-            this.pictureBox2.AccessibleName = "pbx2";
-            this.pictureBox2.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
-            this.pictureBox2.Location = new System.Drawing.Point(522, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 9;
-            this.pictureBox2.TabStop = false;
+            this.cardbtn2.AccessibleName = "pbx2";
+            this.cardbtn2.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+            this.cardbtn2.Location = new System.Drawing.Point(522, 12);
+            this.cardbtn2.Name = "cardbtn2";
+            this.cardbtn2.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn2.TabIndex = 9;
+            this.cardbtn2.TabStop = false;
             // 
-            // pictureBox3
+            // cardbtn3
             // 
-            this.pictureBox3.AccessibleName = "pbx3";
-            this.pictureBox3.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
-            this.pictureBox3.Location = new System.Drawing.Point(336, 12);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 10;
-            this.pictureBox3.TabStop = false;
+            this.cardbtn3.AccessibleName = "pbx3";
+            this.cardbtn3.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
+            this.cardbtn3.Location = new System.Drawing.Point(336, 12);
+            this.cardbtn3.Name = "cardbtn3";
+            this.cardbtn3.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn3.TabIndex = 10;
+            this.cardbtn3.TabStop = false;
             // 
-            // pictureBox4
+            // cardbtn4
             // 
-            this.pictureBox4.AccessibleName = "pbx4";
-            this.pictureBox4.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
-            this.pictureBox4.Location = new System.Drawing.Point(169, 308);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 11;
-            this.pictureBox4.TabStop = false;
+            this.cardbtn4.AccessibleName = "pbx4";
+            this.cardbtn4.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
+            this.cardbtn4.Location = new System.Drawing.Point(169, 308);
+            this.cardbtn4.Name = "cardbtn4";
+            this.cardbtn4.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn4.TabIndex = 11;
+            this.cardbtn4.TabStop = false;
             // 
-            // pictureBox5
+            // cardbtn5
             // 
-            this.pictureBox5.AccessibleName = "pbx5";
-            this.pictureBox5.Image = global::project_memory.Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
-            this.pictureBox5.Location = new System.Drawing.Point(522, 152);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 12;
-            this.pictureBox5.TabStop = false;
+            this.cardbtn5.AccessibleName = "pbx5";
+            this.cardbtn5.Image = global::project_memory.Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
+            this.cardbtn5.Location = new System.Drawing.Point(522, 152);
+            this.cardbtn5.Name = "cardbtn5";
+            this.cardbtn5.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn5.TabIndex = 12;
+            this.cardbtn5.TabStop = false;
             // 
-            // pictureBox6
+            // cardbtn6
             // 
-            this.pictureBox6.AccessibleName = "pbx6";
-            this.pictureBox6.Image = global::project_memory.Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
-            this.pictureBox6.Location = new System.Drawing.Point(522, 308);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 13;
-            this.pictureBox6.TabStop = false;
+            this.cardbtn6.AccessibleName = "pbx6";
+            this.cardbtn6.Image = global::project_memory.Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
+            this.cardbtn6.Location = new System.Drawing.Point(522, 308);
+            this.cardbtn6.Name = "cardbtn6";
+            this.cardbtn6.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn6.TabIndex = 13;
+            this.cardbtn6.TabStop = false;
             // 
-            // pictureBox7
+            // cardbtn7
             // 
-            this.pictureBox7.AccessibleName = "pbx7";
-            this.pictureBox7.Image = global::project_memory.Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
-            this.pictureBox7.Location = new System.Drawing.Point(336, 308);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox7.TabIndex = 14;
-            this.pictureBox7.TabStop = false;
+            this.cardbtn7.AccessibleName = "pbx7";
+            this.cardbtn7.Image = global::project_memory.Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
+            this.cardbtn7.Location = new System.Drawing.Point(336, 308);
+            this.cardbtn7.Name = "cardbtn7";
+            this.cardbtn7.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn7.TabIndex = 14;
+            this.cardbtn7.TabStop = false;
             // 
-            // pictureBox8
+            // cardbtn8
             // 
-            this.pictureBox8.AccessibleName = "pbx8";
-            this.pictureBox8.Image = global::project_memory.Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
-            this.pictureBox8.Location = new System.Drawing.Point(336, 152);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(150, 130);
-            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox8.TabIndex = 15;
-            this.pictureBox8.TabStop = false;
+            this.cardbtn8.AccessibleName = "pbx8";
+            this.cardbtn8.Image = global::project_memory.Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
+            this.cardbtn8.Location = new System.Drawing.Point(336, 152);
+            this.cardbtn8.Name = "cardbtn8";
+            this.cardbtn8.Size = new System.Drawing.Size(150, 130);
+            this.cardbtn8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cardbtn8.TabIndex = 15;
+            this.cardbtn8.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(530, 316);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "label1";
+            // 
+            // currentTurnLabel
+            // 
+            this.currentTurnLabel.AutoSize = true;
+            this.currentTurnLabel.Location = new System.Drawing.Point(49, 69);
+            this.currentTurnLabel.Name = "currentTurnLabel";
+            this.currentTurnLabel.Size = new System.Drawing.Size(44, 16);
+            this.currentTurnLabel.TabIndex = 17;
+            this.currentTurnLabel.Text = "label2";
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Location = new System.Drawing.Point(724, 110);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(44, 16);
+            this.pointsLabel.TabIndex = 18;
+            this.pointsLabel.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.currentTurnLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cardbtn8);
+            this.Controls.Add(this.cardbtn7);
+            this.Controls.Add(this.cardbtn6);
+            this.Controls.Add(this.cardbtn5);
+            this.Controls.Add(this.cardbtn4);
+            this.Controls.Add(this.cardbtn3);
+            this.Controls.Add(this.cardbtn2);
+            this.Controls.Add(this.cardbtn1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Activated += new System.EventHandler(this.Form1_Activated);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardbtn8)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Timer resetTimer;
 <<<<<<< HEAD
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox cardbtn1;
+        private System.Windows.Forms.PictureBox cardbtn2;
+        private System.Windows.Forms.PictureBox cardbtn3;
+        private System.Windows.Forms.PictureBox cardbtn4;
+        private System.Windows.Forms.PictureBox cardbtn5;
+        private System.Windows.Forms.PictureBox cardbtn6;
+        private System.Windows.Forms.PictureBox cardbtn7;
+        private System.Windows.Forms.PictureBox cardbtn8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label currentTurnLabel;
+        private System.Windows.Forms.Timer endTimer;
+        private System.Windows.Forms.Label pointsLabel;
 =======
         private System.Windows.Forms.Label currentTurnLabel;
         private System.Windows.Forms.Label pointsLabel;
--- project memory/project memory/Form1.cs
+++ project memory/project memory/Form1.cs
@@ -11,17 +11,17 @@ using System.Windows.Forms;
 
 namespace project_memory
 {
-    
+
     public partial class Form1 : Form
     {
         private float cardCheck = 0f; // hoeveel kaarten er worden bekeken
         string[] cardnumbers = { "cardbtn1", "cardbtn2", "cardbtn3", "cardbtn4", "cardbtn5", "cardbtn6", "cardbtn7", "cardbtn8" }; //checken welke knop bij welk plaatje in de lijst eronder ligt
-        
-        List<int> picturelist = new List<int>{1, 1, 2, 2, 3, 3, 4, 4};
+
+        List<int> picturelist = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4 };
         List<int> cardpictures = new List<int>(); // waar verschillende plaatjes liggen
 
-        private Button firstpicture;
-        List<Button> correct = new List<Button>();
+        private PictureBox firstpicture;
+        List<PictureBox> correct = new List<PictureBox>();
 
         public int pictureOne;
         public int pictureTwo;
@@ -33,15 +33,15 @@ namespace project_memory
         public int currentTurn = 1;
         public int currentPoints = 0;
 
-        private void resetCard(Button thisB)
+        private void resetCard(PictureBox thisB)
         {
             if (correct.Contains(thisB) == false)
             {
                 thisB.BackgroundImage = null;
             }
         }
-        private void Cardturn(Button btn) //activeert als er op een knop wordt gedrukt
-        {   
+        private void Cardturn(PictureBox btn) //activeert als er op een knop wordt gedrukt
+        {
             if (resetTimer.Enabled == true)
             {
                 timerLength = 0;
@@ -51,66 +51,66 @@ namespace project_memory
                 player.Stream = Properties.Resources.flipcard_91468;
                 player.Play();
 
-              cardCheck += 1f; //er is een kaart omgedraaid
-              if (cardCheck == 1f) 
-              { 
-                  firstpicture = btn;
-              }
-              
-              if (cardCheck > 2f) //als er te veel kaarten zijn omgedraaid gaan alle kaarten uit
-              {
-                 cardCheck = 1f; //er is hierna een kaart over
-                 firstpicture = btn; // stel deze kaart in als firstpicture
+                cardCheck += 1f; //er is een kaart omgedraaid
+                if (cardCheck == 1f)
+                {
+                    firstpicture = btn;
+                }
 
-                  resetCard(cardbtn1);
-                  resetCard(cardbtn2);
-                  resetCard(cardbtn3);
-                  resetCard(cardbtn4);
-                  resetCard(cardbtn5);
-                  resetCard(cardbtn6);
-                  resetCard(cardbtn7);
-                  resetCard(cardbtn8);
-                 currentTurn += 1;
-                 currentTurnLabel.Text = "turn " + currentTurn.ToString();
-              }
+                if (cardCheck > 2f) //als er te veel kaarten zijn omgedraaid gaan alle kaarten uit
+                {
+                    cardCheck = 1f; //er is hierna een kaart over
+                    firstpicture = btn; // stel deze kaart in als firstpicture
+
+                    resetCard(cardbtn1);
+                    resetCard(cardbtn2);
+                    resetCard(cardbtn3);
+                    resetCard(cardbtn4);
+                    resetCard(cardbtn5);
+                    resetCard(cardbtn6);
+                    resetCard(cardbtn7);
+                    resetCard(cardbtn8);
+                    currentTurn += 1;
+                    currentTurnLabel.Text = "turn " + currentTurn.ToString();
+                }
 
 
 
-              for (int i = 0; i < cardnumbers.Length; i++) //voor elk lid in cardnumbers
-              {
-                  if (cardnumbers[i] == btn.Name.ToString()) //als het lid in cardnumbers de geselecteerde knop is...
-                  {
+                for (int i = 0; i < cardnumbers.Length; i++) //voor elk lid in cardnumbers
+                {
+                    if (cardnumbers[i] == btn.Name.ToString()) //als het lid in cardnumbers de geselecteerde knop is...
+                    {
 
-                      if (cardpictures[i] == 1) //...wordt het plaatje van het bijbehorende nummer in cardpictures laten zien
-                      {
-                          btn.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
-                      }
-                      if (cardpictures[i] == 2)
-                      {
-                          btn.BackgroundImage = Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
-                      }
-                      if (cardpictures[i] == 3)
-                      {
-                          btn.BackgroundImage = Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
-                      }
-                      if (cardpictures[i] == 4)
-                      {
-                          btn.BackgroundImage = Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
-                      }
+                        if (cardpictures[i] == 1) //...wordt het plaatje van het bijbehorende nummer in cardpictures laten zien
+                        {
+                            btn.BackgroundImage = Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+                        }
+                        if (cardpictures[i] == 2)
+                        {
+                            btn.BackgroundImage = Properties.Resources.Dries_en_ik___Verhalen_van_vroege_r_;
+                        }
+                        if (cardpictures[i] == 3)
+                        {
+                            btn.BackgroundImage = Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
+                        }
+                        if (cardpictures[i] == 4)
+                        {
+                            btn.BackgroundImage = Properties.Resources.Wat_je_kan_leren_van_een_kip___Hilde_Schoonjans;
+                        }
 
-                      if (cardCheck == 1)
-                      {
-                          pictureOne = cardpictures[i];
-                      }
-                      else if (cardCheck == 2) 
-                      {
-                          pictureTwo = cardpictures[i];
-                      }
-                  }
-              }
+                        if (cardCheck == 1)
+                        {
+                            pictureOne = cardpictures[i];
+                        }
+                        else if (cardCheck == 2)
+                        {
+                            pictureTwo = cardpictures[i];
+                        }
+                    }
+                }
 
-              if (cardCheck == 2f)
-              {
+                if (cardCheck == 2f)
+                {
 
                     if (pictureOne == pictureTwo)
                     {
@@ -124,12 +124,12 @@ namespace project_memory
                         {
                             timerLength = 0;
                             endTimer.Start();
-                            
+
                         }
                     }
                     resetTimer.Start();
                 }
-              }
+            }
         }
 
         private void Form1_Activated(object sender, EventArgs e)
@@ -219,3 +219,7 @@ namespace project_memory
                 Application.Exit();
             }
         }
+    }
+}
+
+        
