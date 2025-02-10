06312bd557cc979d37e9b53174a6a8256ef80842
joelmoltnova <joelmol2008@gmail.com>
2025-02-10 13:45:09 +0100

    PictureBox Update
    In plaats van buttons zijn er nu picturebox.

--- project memory/project memory/Form1.Designer.cs
+++ project memory/project memory/Form1.Designer.cs
@@ -29,125 +29,101 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.cardbtn1 = new System.Windows.Forms.Button();
-            this.cardbtn2 = new System.Windows.Forms.Button();
-            this.cardbtn3 = new System.Windows.Forms.Button();
-            this.cardbtn4 = new System.Windows.Forms.Button();
-            this.cardbtn5 = new System.Windows.Forms.Button();
-            this.cardbtn6 = new System.Windows.Forms.Button();
-            this.cardbtn7 = new System.Windows.Forms.Button();
-            this.cardbtn8 = new System.Windows.Forms.Button();
             this.resetTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
-            // 
-            // cardbtn1
-            // 
-            this.cardbtn1.Location = new System.Drawing.Point(170, 49);
-            this.cardbtn1.Name = "cardbtn1";
-            this.cardbtn1.Size = new System.Drawing.Size(111, 96);
-            this.cardbtn1.TabIndex = 0;
-            this.cardbtn1.UseVisualStyleBackColor = true;
-            this.cardbtn1.Click += new System.EventHandler(this.cardClick1);
-            // 
-            // cardbtn2
-            // 
-            this.cardbtn2.Location = new System.Drawing.Point(348, 50);
-            this.cardbtn2.Name = "cardbtn2";
-            this.cardbtn2.Size = new System.Drawing.Size(110, 97);
-            this.cardbtn2.TabIndex = 1;
-            this.cardbtn2.UseVisualStyleBackColor = true;
-            this.cardbtn2.Click += new System.EventHandler(this.cardbtn2_Click);
-            // 
-            // cardbtn3
-            // 
-            this.cardbtn3.Location = new System.Drawing.Point(527, 49);
-            this.cardbtn3.Name = "cardbtn3";
-            this.cardbtn3.Size = new System.Drawing.Size(105, 96);
-            this.cardbtn3.TabIndex = 2;
-            this.cardbtn3.UseVisualStyleBackColor = true;
-            this.cardbtn3.Click += new System.EventHandler(this.cardbtn3_Click);
-            // 
-            // cardbtn4
-            // 
-            this.cardbtn4.Location = new System.Drawing.Point(170, 171);
-            this.cardbtn4.Name = "cardbtn4";
-            this.cardbtn4.Size = new System.Drawing.Size(100, 89);
-            this.cardbtn4.TabIndex = 3;
-            this.cardbtn4.UseVisualStyleBackColor = true;
-            this.cardbtn4.Click += new System.EventHandler(this.cardbtn4_Click);
-            // 
-            // cardbtn5
-            // 
-            this.cardbtn5.Location = new System.Drawing.Point(348, 171);
-            this.cardbtn5.Name = "cardbtn5";
-            this.cardbtn5.Size = new System.Drawing.Size(110, 89);
-            this.cardbtn5.TabIndex = 4;
-            this.cardbtn5.UseVisualStyleBackColor = true;
-            this.cardbtn5.Click += new System.EventHandler(this.cardbtn5_Click);
-            // 
-            // cardbtn6
-            // 
-            this.cardbtn6.Location = new System.Drawing.Point(527, 171);
-            this.cardbtn6.Name = "cardbtn6";
-            this.cardbtn6.Size = new System.Drawing.Size(105, 89);
-            this.cardbtn6.TabIndex = 5;
-            this.cardbtn6.UseVisualStyleBackColor = true;
-            this.cardbtn6.Click += new System.EventHandler(this.cardbtn6_Click);
-            // 
-            // cardbtn7
-            // 
-            this.cardbtn7.Location = new System.Drawing.Point(266, 290);
-            this.cardbtn7.Name = "cardbtn7";
-            this.cardbtn7.Size = new System.Drawing.Size(98, 87);
-            this.cardbtn7.TabIndex = 6;
-            this.cardbtn7.UseVisualStyleBackColor = true;
-            this.cardbtn7.Click += new System.EventHandler(this.cardbtn7_Click);
-            // 
-            // cardbtn8
-            // 
-            this.cardbtn8.Location = new System.Drawing.Point(443, 290);
-            this.cardbtn8.Name = "cardbtn8";
-            this.cardbtn8.Size = new System.Drawing.Size(98, 88);
-            this.cardbtn8.TabIndex = 7;
-            this.cardbtn8.UseVisualStyleBackColor = true;
-            this.cardbtn8.Click += new System.EventHandler(this.cardbtn8_Click);
             // 
             // resetTimer
             // 
             this.resetTimer.Interval = 1;
             this.resetTimer.Tick += new System.EventHandler(this.resetTimer_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(46, 306);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
+            this.pictureBox3.Location = new System.Drawing.Point(340, 214);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::project_memory.Properties.Resources.Goofy_Horse___Horses__Animals__Roman;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 170);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::project_memory.Properties.Resources.Verdwijnt_de_koe_uit_ons_landschap_;
+            this.pictureBox5.Location = new System.Drawing.Point(184, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 130);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cardbtn8);
-            this.Controls.Add(this.cardbtn7);
-            this.Controls.Add(this.cardbtn6);
-            this.Controls.Add(this.cardbtn5);
-            this.Controls.Add(this.cardbtn4);
-            this.Controls.Add(this.cardbtn3);
-            this.Controls.Add(this.cardbtn2);
-            this.Controls.Add(this.cardbtn1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Activated += new System.EventHandler(this.Form1_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button cardbtn1;
-        private System.Windows.Forms.Button cardbtn2;
-        private System.Windows.Forms.Button cardbtn3;
-        private System.Windows.Forms.Button cardbtn4;
-        private System.Windows.Forms.Button cardbtn5;
-        private System.Windows.Forms.Button cardbtn6;
-        private System.Windows.Forms.Button cardbtn7;
-        private System.Windows.Forms.Button cardbtn8;
         private System.Windows.Forms.Timer resetTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
     }
 }
 
