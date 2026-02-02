a9560063f2c68601e964e7344596201294c00964
dennis5132 <dennisvkrevel@gmail.com>
2026-02-02 11:33:32 +0100

    endstate

---
+++ libraryTest/libraryTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            private void TelAantalKlinkers(string woord)
+        {
+            woord = "krant";
+            Console.WriteLine(woord[0]);
+        }
+        }
+    }
+}
